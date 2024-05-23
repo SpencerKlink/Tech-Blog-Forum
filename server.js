400fb8687c921b37e236d9da945af3b10b05e37d
Spencer Klinkowize <spencerk@spencers-mbp-m3.lan>
2024-05-23 18:09:31 -0400

    handlebars and server.js fix

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const sessions = require('express-session');
 const exphbs = require('express-handlebars');
+const Sequelize = require('sequelize'); 
 const SequelizeStore = require('connect-session-sequelize')(sessions.Store);
-const path = require('path');
-const sequelize = require('./config/config'); 
-const routes = require('./controllers');
+const { sequelize } = require('./models'); 
+const routes = require('./controllers'); 
+const helpers = require('./utils/helpers'); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-app.engine('handlebars', exphbs.engine());
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 const sess = {
